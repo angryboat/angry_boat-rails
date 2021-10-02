bc639dfafea420fca02e2bbf5e4ef3d71dc86be0
Maddie Schipper <me@maddiesch.com>
2021-10-02 17:10:31 +0000

    Fixes for auto loading

---
+++ lib/angry_boat-rails.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# This file exists only for Bundler auto-loading
+
+require 'angry_boat'
--- lib/angry_boat/rspec/graphql_example_group.rb
+++ lib/angry_boat/rspec/graphql_example_group.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
+require 'action_controller'
+require 'action_dispatch'
+require 'action_dispatch/testing/integration'
+
 module AngryBoat
   module RSpec
     module GraphQLExampleGroup
-      def execute(query, variables: {}, operation_name: nil)
-        headers = self.class.metadata.fetch(:headers, {})
+      include ActionDispatch::Integration::Runner
+
+      def app
+        ::Rails.application
+      end
+
+      def execute(query, variables: {}, operation_name: nil, request_headers: {})
+        header_provider = self.class.metadata.fetch(:headers, -> { {} })
+        headers = instance_exec(&header_provider).merge(request_headers)
         headers['Content-Type'] ||= 'application/json'
         headers['Accept'] ||= 'application/json'
 
@@ -17,8 +28,8 @@ module AngryBoat
         post(self.class.metadata.fetch(:graph_path), params: JSON.dump(params), headers: headers)
       end
 
-      def response_value(key_path)
-        response.parsed_body.dig(*AngryBoat.key_path(key_path))
+      def data_value_at(key_path)
+        response.parsed_body.fetch('data', {}).dig(*AngryBoat.key_path(key_path))
       end
 
       def self.included(klass)
@@ -44,6 +55,6 @@ RSpec.configure do |config|
     metadata[:graph_path] = '/graphql'
   end
 
-  config.include(RSpec::Rails::RequestExampleGroup, type: :graphql)
+  # config.include(RSpec::Rails::RequestExampleGroup, type: :graphql)
   config.include(AngryBoat::RSpec::GraphQLExampleGroup, type: :graphql)
 end
